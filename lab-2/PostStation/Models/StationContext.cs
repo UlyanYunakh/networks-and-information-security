9c948f3322aab7ede2713d296daa0987b18a1f66
Ulyan Yunakh <44743429+UlyanYunakh@users.noreply.github.com>
2021-03-07 12:49:25 +0300

    Lab 2 (#3)
    * start project
    
    * add MVC
    
    * add models for database
    
    * add database (postresql)
    
    * add navigation
    
    * navigation style fix
    
    * change models for database
    
    * add actions for developers and countries
    
    * add countries views
    
    * add developers views
    
    * navbar style change
    
    * layout and imports fix
    
    * lauout style change
    
    * actions and view for platforms
    
    * add actions and views for games
    
    * project renaming
    
    * navbar style change
    
    * add actions and views for main
    
    * view fix

---
+++ lab-2/PostStation/Models/StationContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PostStation.Models
+{
+    public class StationContext : DbContext
+    {
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public StationContext(DbContextOptions<StationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
